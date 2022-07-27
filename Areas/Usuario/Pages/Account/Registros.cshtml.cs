c1e949e0e9e77d3df35fac74ecd1d7d34383af30
bfjose08 <bfjose26@gmail.com>
2022-07-26 19:28:20 -0500

    modelo de base de datos usando DataAnnotations, confirmación de contraseña

--- Areas/Usuario/Pages/Account/Registros.cshtml.cs
+++ Areas/Usuario/Pages/Account/Registros.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +10,34 @@ namespace Paginas_Razor.Areas.Usuario.Pages.Account
 {
     public class RegistrosModel : PageModel
     {
-        public void OnGet()
+        //public string data { get; set; }
+        public void OnGet(string data)
         {
+            //this.data = data;
+        }
+        [BindProperty]
+        public InputModel Input { get; set; }// "Input" permite acceder a las propiedades del modelo.
+        public class InputModel
+        {
+            [Required]//no puede ser nulo
+            [Display(Name ="Name")]//lo que el usuario visualiza en la pagina
+            public string Name { get; set; }//campo dela base de datos
+
+            [Required]
+            [EmailAddress]//propiedad que define que es uncorreo
+            [Display(Name = "Mail")]
+            public string Mail { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]//da formato de contraseña al campo ******
+            [Display(Name ="Contraseña")]
+            [StringLength(100, ErrorMessage ="El numero de caracteres de {0} debe ser al menos {2}",MinimumLength =6)]//limita los caracteres que el usuario puede usar
+            public string Password { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage ="The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
         }
     }
 }
